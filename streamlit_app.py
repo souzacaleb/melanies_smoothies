9080a41b413fda53d226986f5a63d5c89a6b1f0e
souzacaleb <41654636+souzacaleb@users.noreply.github.com>
2024-03-14 20:16:18 -0300

    Update streamlit_app.py
    fixing identention

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-	fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -44,5 +44,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 	    
         st.success('Your Smoothie is ordered!', icon="✅")
-
-
