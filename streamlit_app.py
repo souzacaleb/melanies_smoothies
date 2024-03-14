fba73f93781652ac76c7d6d0eddb158a51002190
souzacaleb <41654636+souzacaleb@users.noreply.github.com>
2024-03-14 20:07:25 -0300

    Update streamlit_app.py
    expose json data

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-
 # Write directly to the app
 st.title("Customize Your Smoothie:balloon:")
 st.write(
@@ -45,3 +42,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 	    
         st.success('Your Smoothie is ordered!', icon="✅")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
